a42365ff4deb60a00434bd06be2f01554f7dfe86
Reid Vandewiele <reid@puppetlabs.com>
2015-11-24 16:05:39 -0800

    Add nimbus configuration

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ To install and manage a lab computer:
 1. On the lab computer, open Windows PowerShell
 2. Run the following command
 
-    powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('http://bit.ly/1kEgbuH')"
+    powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('http://git.io/vBnll')"
--- scripts/install.ps1
+++ scripts/install.ps1
@@ -2,31 +2,38 @@
 # from the master's pe_repo by downloading it to C:\tmp first and then running
 # msiexec on it from there.
 
-$puppet_master_server = "localhost"
-#$msi_source = "http://downloads.puppetlabs.com/windows/puppet-agent-x64-latest.msi"
-#$msi_dest = "C:\Windows\temp\puppet-agent-x64.msi"
-$msi_source = "http://downloads.puppetlabs.com/windows/puppet-agent-x86-latest.msi"
-$msi_dest = "C:\Windows\temp\puppet-agent-x86.msi"
-
-
-# Determine system hostname and primary DNS suffix to determine certname
-Write-Host "Determining system name"
-$objIPProperties = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties()
-$name_components = @($objIPProperties.HostName, $objIPProperties.DomainName) | ? {$_}
-$certname = ($name_components -Join ".").ToLower()
-Write-Host "System name is $certname"
-
 Function Http-Get { Param($url, $file)
   $webclient = New-Object system.net.webclient
   $webclient.DownloadFile($url,$file)
 }
 
+# Get system information and set variables
+$puppet_master_server = "localhost"
+#$msi_source = "http://downloads.puppetlabs.com/windows/puppet-agent-x64-latest.msi"
+#$msi_dest = "C:\Windows\temp\puppet-agent-x64.msi"
+$objIPProperties = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties()
+$name_components = @($objIPProperties.HostName, $objIPProperties.DomainName) | ? {$_}
+$certname = ($name_components -Join ".").ToLower()
+$msi_source = "http://downloads.puppetlabs.com/windows/puppet-agent-x86-latest.msi"
+$msi_dest = "C:\Windows\temp\puppet-agent-x86.msi"
+$msiexec_path = "C:\Windows\System32\msiexec.exe"
+$msiexec_args = "/qn /i $msi_dest PUPPET_AGENT_CERTNAME=$certname PUPPET_MASTER_SERVER=localhost PUPPET_AGENT_STARTUP_MODE=Disabled"
+$puppet_path = "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat"
+$gem_path = "C:\Program Files\Puppet Labs\Puppet\sys\ruby\bin\gem"
+
+Write-Host "System name is $certname"
+
 Write-Host "Downloading puppet agent from $msi_source"
 #Http-Get -url $msi_source -file $msi_dest
 
 Write-Host "Installing puppet agent"
-$msiexec_path = "C:\Windows\System32\msiexec.exe"
-$msiexec_args = "/qn /i $msi_dest PUPPET_AGENT_CERTNAME=$certname PUPPET_MASTER_SERVER=localhost PUPPET_AGENT_STARTUP_MODE=Disabled"
-$process = Start-Process -FilePath $msiexec_path -ArgumentList $msiexec_args -Wait -PassThru
+Start-Process -FilePath $msiexec_path -ArgumentList $msiexec_args -Wait
 
+Write-Host "Installing hocon gem for nimbus module"
+Start-Process -FilePath $gem_path -ArgumentList "install hocon --no-ri --no-rdoc" -Wait
 
+Write-Host "Installing nimbus module"
+Start-Process -FilePath $puppet_path -ArgumentList "module install tse/nimbus" -Wait
+
+Write-Host "Configuring system with Puppet Nimbus"
+Start-Process -FilePath $puppet_path -ArgumentList "nimbus apply https://git.io/vBnP6" -Wait
