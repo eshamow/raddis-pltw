5f62c3ff124c1195087e6c34f1e80f76af1b837a
Reid Vandewiele <reid@puppetlabs.com>
2015-11-26 00:48:19 -0800

    Add "Run Puppet Nimbus" shortcut to start menu
    This also serves as a first functional test of whether or not this is a
    working means of deploying changes.

--- manifests/puppet/baseline.pp
+++ manifests/puppet/baseline.pp
@@ -1,5 +1,5 @@
 class pltw::puppet::baseline {
-  $config = $pltw::params::nimbus_config
+  include pltw
 
   # Work around a heinous error printing bug on Windows.
   file { 'C:/Program Files/Puppet Labs/Puppet/puppet/lib/puppet/util/monkey_patches.rb':
@@ -8,9 +8,11 @@ class pltw::puppet::baseline {
   }
 
   # Put a "Run Puppet Nimbus" link in the start menu
-  file { 'C:/ProgramData/Microsoft/Windows/Start Menu/Programs/Puppet/Run Puppet Nimbus.lnk':
-    ensure  => file,
-    content => epp('pltw/puppet/shortcut.lnk.epp', { 'config' => $config }),
+  shortcut { 'C:/ProgramData/Microsoft/Windows/Start Menu/Programs/Puppet/Run Puppet Nimbus.lnk':
+    target            => 'C:/Program Files/Puppet Labs/Puppet/bin/puppet.bat',
+    arguments         => "nimbus apply ${pltw::nimbus_config}",
+    working_directory => 'C:/Program Files/Puppet Labs/Puppet/bin',
+    description       => 'Run Puppet Nimbus on-demand',
   }
 
 }
