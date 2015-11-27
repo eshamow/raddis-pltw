9f0619597e8dbc13aaa6478a6725e3732bbd49d2
Reid Vandewiele <reid@puppetlabs.com>
2015-11-26 21:42:04 -0800

    Add SSL certificate management for nginx

--- manifests/puppetdb.pp
+++ manifests/puppetdb.pp
@@ -3,15 +3,25 @@ class pltw::puppetdb {
   include puppetdb
   include nginx
 
-  nginx::resource::upstream { 'puppetdb':
+  nginx::resource::upstream { 'puppetdb-app':
     members => ['localhost:8080'],
   }
 
-  nginx::resource::vhost { 'puppetdb':
-    proxy       => 'http://puppetdb',
+  openssl::certificate::x509 { $::fqdn:
+    country      => 'US',
+    organization => 'BEH',
+    commonname   => $::fqdn,
+    base_dir     => '/etc/ssl/certs',
+    notify       => Nginx::Resource::Vhost['puppetdb-proxy'],
+  }
+
+  nginx::resource::vhost { 'puppetdb-proxy':
+    proxy       => 'http://puppetdb-app',
     listen_port => 8081,
     ssl_port    => 8081,
     ssl         => true,
+    ssl_cert    => "/etc/ssl/certs/${::fqdn}.crt",
+    ssl_key     => "/etc/ssl/certs/${::fqdn}.key",
   }
 
 }
