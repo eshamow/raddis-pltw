71f92d27420d94fd23b6a4d2962ff0594c29fa18
Reid Vandewiele <reid@puppetlabs.com>
2015-12-02 08:33:24 -0800

    Just use master code

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,9 +6,9 @@ class pltw ($nimbus_config) {
   include pltw::puppet::reporting
 
   # System-level configuration
-  include stdlib::stages
-  class { 'pltw::baseline::setup':  stage => 'setup'  }
-  class { 'pltw::baseline::deploy': stage => 'deploy' }
+  #include stdlib::stages
+  #class { 'pltw::baseline::setup':  stage => 'setup'  }
+  #class { 'pltw::baseline::deploy': stage => 'deploy' }
 
   # Lab computer desired state configuration
   include pltw::websites
