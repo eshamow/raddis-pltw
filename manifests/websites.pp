f8ef2dd12e6f78ccde4922e3b9c2c6f769ccb60d
Reid Vandewiele <reid@puppetlabs.com>
2015-12-01 23:04:15 -0800

    Add conditional website blocking
    Based on given time begin/end blocks.

--- manifests/websites.pp
+++ manifests/websites.pp
@@ -1,10 +1,17 @@
-class pltw::websites ($blocked = []) {
+class pltw::websites (
+  $blocked = [],
+  $block_start_time = 0,
+  $block_end_time   = 0,
+) {
 
-  $blocked.each |$site| {
-    host { $site:
-      ensure  => present,
-      ip      => '127.0.0.1', # point to localhost instead of web address ip
-      comment => 'pltw::website_block',
+  $current_time = strftime("%H%M") + 0
+  if ($current_time >= $block_start_time and $current_time < $block_end_time) {
+    $blocked.each |$site| {
+      host { $site:
+        ensure  => present,
+        ip      => '127.0.0.1', # point to localhost instead of web address ip
+        comment => 'pltw::website_block',
+      }
     }
   }
 
